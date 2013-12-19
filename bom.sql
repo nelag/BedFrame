20e3ae32e4f52682c1279b1db900dcc2d04757d7
Galen Church <galenechurch@gmail.com>
2013-12-19 13:28:22 -0500

    SwitchAutoSave

--- bom.sql
+++ bom.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 192.237.246.191    Database: bndr_s001
 -- ------------------------------------------------------
--- Server version	5.1.69-log
+-- Server version	5.1.69
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -81,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-12-19 11:39:24
+-- Dump completed on 2013-12-19 13:28:22
